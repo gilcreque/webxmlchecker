ae58fb8a32d9200fc8993a0ffc8443a6bc3e93db
gilcreque <me@gilcreque.com>
2013-03-20 17:01:00 -0400

    Broke check_question into separate functions to reuse for later additions

--- webxmlchecker.py
+++ webxmlchecker.py
@@ -7,150 +7,196 @@ from datetime import datetime
 
 import websurvey
 
+def check_question_validate_checkbox(question):
 
-def checkxml(question, checkFile, survey):
-
-    questionWarning = False
-    checkFileText = ""
-
-    validCheckedType = ['Category', 'Multiple', 'Numeric', 'OpenEnd',
+    _check_file_text = ""
+    _valid_checked_type = ['Category', 'Multiple', 'Numeric', 'OpenEnd',
                         'OpenEndBig']
-    validNotCheckedType = ['Display', 'Calculation', 'Random']
+    _valid_not_checked_type = ['Display', 'Calculation', 'Random']
 
-    if question.type in validCheckedType:
+    if question.type in _valid_checked_type:
         if not question.validate:
-            checkFileText += (question.varname + " is a " + question.type +
+            _check_file_text += (question.varname + " is a " + question.type +
                             " type and validate is not checked\n")
-            questionWarning = True
-    elif question.type in validNotCheckedType:
+            return True, _check_file_text
+    elif question.type in _valid_not_checked_type:
         if question.validate:
-            checkFileText += (question.varname + " is a " + question.type +
+            _check_file_text += (question.varname + " is a " + question.type +
                             " type and validate is checked\n")
-            questionWarning = True
+            return True, _check_file_text
+    return False,
 
-    locationGTZero = ['Category', 'Multiple', 'Numeric', 'OpenEnd',
+
+def check_question_location(question):
+
+    _check_file_text = ""
+    _location_gt_zero = ['Category', 'Multiple', 'Numeric', 'OpenEnd',
                       'OpenEndBig', 'Calculation', 'Random']
-    locationZero = ['Display']
+    _location_zero = ['Display']
 
-    if question.type in locationGTZero and question.displayType != "Grid":
+    if question.type in _location_gt_zero and question.displayType != "Grid":
         if question.location < 1:
-            checkFileText += (question.varname + " is a " + question.type +
+            _check_file_text += (question.varname + " is a " + question.type +
                             " type and location is less than one\n")
-            questionWarning = True
-    elif (question.type in locationZero) or (question.type == "Category" and
+            return True, _check_file_text
+    elif (question.type in _location_zero) or (question.type == "Category" and
                                             question.displayType == "Grid"):
         if question.location != 0:
-            checkFileText += (question.varname + " is a Grid Display and " +
+            _check_file_text += (question.varname + " is a Grid Display and " +
                             "location is not zero\n")
-            questionWarning = True
+            return True, _check_file_text
+    return False,
 
-    lengthGTZero = ['Category', 'Multiple', 'Numeric',
+
+def check_question_length(question):
+
+    _check_file_text = ""
+    _length_gt_zero = ['Category', 'Multiple', 'Numeric',
                     'Calculation', 'Random']
-    lengthZero = ['Display']
-    lengthDefault = {'OpenEnd': 2, 'OpenEndBig': 20}
+    _length_zero = ['Display']
+    _length_default = {'OpenEnd': 2, 'OpenEndBig': 20}
 
-    if question.type in lengthGTZero and question.displayType != "Grid":
+    if question.type in _length_gt_zero and question.displayType != "Grid":
         if question.length < 1:
-            checkFileText += (question.varname + " is a " + question.type +
+            _check_file_text += (question.varname + " is a " + question.type +
                             " type and length is less than one\n")
-            questionWarning = True
-    elif (question.type in lengthZero) or (question.type == "Category" and
+            return True, _check_file_text
+    elif (question.type in _length_zero) or (question.type == "Category" and
                                             question.displayType == "Grid"):
         if question.length != 0:
-            checkFileText += (question.varname + " is a Grid Display and " + 
+            _check_file_text += (question.varname + " is a Grid Display and " + 
                             "length is not zero\n")
-            questionWarning = True
-    for k, v in lengthDefault.iteritems():
+            return True, _check_file_text
+    for k, v in _length_default.iteritems():
         if question.type == k and question.length != v:
-            checkFileText += (question.varname + " is a " + k +
+            _check_file_text += (question.varname + " is a " + k +
                      " type and length is not default(" + str(v) + ")\n")
-            questionWarning = True
+            return True, _check_file_text
+    return False,
 
-    numMultGTZero = ['Category', 'Multiple', 'Numeric',
+
+def check_question_nummult(question):
+
+    _check_file_text = ""
+    _nummult_gt_zero = ['Category', 'Multiple', 'Numeric',
                     'Calculation']
-    numMultDefault = {'OpenEnd': 2, 'OpenEndBig': 10, 'Display': 1,
+    _nummult_default = {'OpenEnd': 2, 'OpenEndBig': 10, 'Display': 1,
                     'Random': 1}
 
-    if question.type in numMultGTZero:
+    if question.type in _nummult_gt_zero:
         if question.num_responses < 1:
-            checkFileText += (question.varname + " is a " + question.type +
+            _check_file_text += (question.varname + " is a " + question.type +
                             " type and NumMult is less than one\n")
-            questionWarning = True
-    for k, v in numMultDefault.iteritems():
+            _question_warning_flag = True
+            return True, _check_file_text
+    for k, v in _nummult_default.iteritems():
         if question.type == k and question.num_responses != v:
-            checkFileText += (question.varname + " is a " + k +
+            _check_file_text += (question.varname + " is a " + k +
                      " type and NumMult is not default(" + str(v) + ")\n")
-            questionWarning = True
+            _question_warning_flag = True
+            return True, _check_file_text
+    return False,
 
-    if question.filter is None:
-        checkFileText += (question.varname + " has a blank filter\n")
-        questionWarning = True
-    elif question.filter:
-        filterGroups = re.findall(
-                         '([A-Za-z+][A-Za-z0-9]*\([A-Za-z0-9,-]+\))',
-                         question.filter)
-        #print question.varname, ' '.join(filterGroups)
-        for logic in filterGroups:
-            notInRange = False
-            notInRangeList = []
-            questionMatch = re.search(
-                          '([A-Za-z+][A-Za-z0-9]*(?=\([A-Za-z0-9,-]+\)))'
-                          , logic).group(0)
-            try:
-                questionCheck = getattr (survey, questionMatch)
-            except (AttributeError):
-                checkFileText += (question.varname + " filter error: " + "\"" +
-                                questionMatch +
+
+def check_logic(name, typename, logic, survey):
+
+    _check_file_text = ""
+    _logic_groups = re.findall(
+                     '([A-Za-z+][A-Za-z0-9]*\([A-Za-z0-9,-]+\))', logic)
+    #print name, ' '.join(_logic_groups)
+    for logic in _logic_groups:
+        notInRange = False
+        notInRangeList = []
+        questionMatch = re.search(
+                      '([A-Za-z+][A-Za-z0-9]*(?=\([A-Za-z0-9,-]+\)))'
+                      , logic).group(0)
+        try:
+            questionCheck = getattr (survey, questionMatch)
+        except (AttributeError):
+            _check_file_text += (name + " " + typename + " error: " + 
+                                "\"" + questionMatch +
                                 "\" is not a valid question\n")
-                questionWarning = True
-                break
-            
-            punchesMatch = re.split(",", re.search('(?<=\()([A-Za-z0-9,-]+)',
-                            logic).group(0))
-            for punches in punchesMatch:
-                findRange = re.search('([0-9]+)(-)([0-9]+)', punches)
-                if findRange:
-                    punchLength = len(findRange.group(3))
-                    rangeLow = int(findRange.group(1))
-                    rangeHigh = int(findRange.group(3))+1
-                    punchesMatch.remove(punches)
-                    for i in range (rangeLow, rangeHigh): 
-                        punchesMatch.append(str(i).zfill(punchLength))
-            #print punchesMatch
-            for punches in punchesMatch:
-                try:
-                    getattr (getattr (questionCheck, "responses"), "_"+punches)
-                except (AttributeError):
-                    if questionCheck.type == "Numeric":
-                        try:
-                            checkPunches = int(punches)
-                            numRange = range (questionCheck.min, 
-                                              questionCheck.max+1)
-                            #print checkPunches, numRange
-                            if checkPunches not in numRange:
-                                raise ValueError
-                        except ValueError:
-                            notInRange = True
-                            notInRangeList.append(punches)
-                    else:
-                        checkFileText += (question.varname + " filter error: " +
-                                        "\"" + punches +
+            return True, _check_file_text
+            break
+        
+        punchesMatch = re.split(",", re.search('(?<=\()([A-Za-z0-9,-]+)',
+                        logic).group(0))
+        for punches in punchesMatch:
+            findRange = re.search('([0-9]+)(-)([0-9]+)', punches)
+            if findRange:
+                punchLength = len(findRange.group(3))
+                rangeLow = int(findRange.group(1))
+                rangeHigh = int(findRange.group(3))+1
+                punchesMatch.remove(punches)
+                for i in range (rangeLow, rangeHigh): 
+                    punchesMatch.append(str(i).zfill(punchLength))
+        #print punchesMatch
+        for punches in punchesMatch:
+            try:
+                getattr (getattr (questionCheck, "responses"), "_"+punches)
+            except (AttributeError):
+                if questionCheck.type == "Numeric":
+                    try:
+                        checkPunches = int(punches)
+                        numRange = range (questionCheck.min, 
+                                          questionCheck.max+1)
+                        #print checkPunches, numRange
+                        if checkPunches not in numRange:
+                            raise ValueError
+                    except ValueError:
+                        notInRange = True
+                        notInRangeList.append(punches)
+                else:
+                    _check_file_text += (name + " " + typename +
+                                        " error: " + "\"" + punches +
                                         "\" is not a valid response for " +
                                         questionMatch + "\n")
-                        questionWarning = True
-            if notInRange:
-                checkFileText += (question.varname + " filter error: \"" +
+                    return True, _check_file_text
+        if notInRange:
+            _check_file_text += (name + " " + typename + " error: \"" +
                                 ", ".join(notInRangeList) +
-                                "\" not within range for " + questionMatch +
-                                "\n")
-                questionWarning = True
-    
-    if questionWarning == True:
+                                "\" not within range for " +
+                                questionMatch + "\n")
+            return True, _check_file_text
+    return False,
+        
+        
+def check_filter_logic(question, survey):
+    if question.filter is None:
+        _check_file_text = (question.varname + " has a blank filter\n")
+        return True, _check_file_text
+    elif question.filter:
+        return check_logic(question.varname, "filter", question.filter, survey)
+    else:
+        return False,
+
+def check_question(question, checkFile, survey):
+
+    _question_warning_flag = False
+    _check_file_text = ""
+
+    validate = check_question_validate_checkbox(question)
+    location = check_question_location(question)
+    length = check_question_length(question)
+    nummult = check_question_nummult(question)
+    filterlogic = check_filter_logic(question, survey)
+
+    check_question_list = [validate, location, length, nummult, filterlogic]
+
+    for i, checks in enumerate(check_question_list):
+        if checks[0]:
+            _question_warning_flag = True
+            _check_file_text += checks[1]          
+    if _question_warning_flag == True:
         dashText = ("-"*((78-len(question.varname))/2)) + " "
         dashText += question.varname + " "
         dashText += ("-"*(((78-len(question.varname)) + 2 / 2) / 2))   
-        
-        checkFile.write("\n" + dashText + "\n" + checkFileText + "\n")
+        checkFile.write("\n" + dashText + "\n" + _check_file_text + "\n")
+
+
+def check_quota_logic(quota, survey):
+    check_logic(quota.name, "quota", quota.logic, survey)
+    pass
 
 
 def main():
@@ -186,7 +232,7 @@ def main():
 
         try:
             for question in survey.questionList:
-                checkxml(question, checkFile, survey)
+                check_question(question, checkFile, survey)
 
         finally:
             checkFile.close()
